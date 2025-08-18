c3bb203a9aaab3b7f37cc8089fbb3d5b9b0a465c
Antonio Pedro <153738631+antoniopedroaraujo@users.noreply.github.com>
2025-08-18 17:46:51 -0300

    Delete src directory

--- src/builders/VeiculoListBuilder.java
+++
@@ -1,9 +0,0 @@
-package builders;
-
-import veiculos.*;
-
-import java.util.List;
-
-public interface VeiculoListBuilder {
-    List<Veiculo> buildVeiculoList(List<Carro> carros, List<Onibus> onibus, List<Caminhao> caminhoes);
-}
