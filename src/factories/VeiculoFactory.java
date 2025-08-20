fca52b4b54c4d0c541144e195e530d3d4f8371fe
Antonio Pedro <153738631+antoniopedroaraujo@users.noreply.github.com>
2025-08-19 22:58:03 -0300

    Delete src directory

--- src/factories/VeiculoFactory.java
+++
@@ -1,8 +0,0 @@
-package factories;
-
-import veiculos.Veiculo;
-
-public interface VeiculoFactory {
-    Veiculo createVeiculo(String placa, String cor, long quilometragem, double valorEmReais);
-
-}
